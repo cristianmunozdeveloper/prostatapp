c7d578a7833c95c99dbca7ceb030cc0c1dc1d29d
Cristian Muñoz <cristian9016@gmail.com>
2022-07-06 19:06:19 -0500

    project created

---
+++ app/src/main/java/com/cristiansofthouse/prostatapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cristiansofthouse.prostatapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
