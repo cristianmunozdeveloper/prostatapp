6a62ba8b43d0c7a8775b6cfce4cf8703c6948884
juanse03 <sejuan03@gmail.com>
2022-07-07 18:12:56 -0500

    Prostatest creation started

---
+++ prostatest/src/main/java/com/cristiansofthouse/prostatest/ProstaTestActivity.kt
@@ -0,0 +1,11 @@
+package com.cristiansofthouse.prostatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProstaTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prosta_test)
+    }
+}
