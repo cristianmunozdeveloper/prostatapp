1fbd75168c8516f70d314665d27990dc1d12931c
Cristian Muñoz <cristian9016@gmail.com>
2022-07-08 12:34:14 -0500

    styles fixed

--- tabutest/src/main/java/com/cristiansofthouse/tabutest/TabuTestActivity.kt
+++ tabutest/src/main/java/com/cristiansofthouse/tabutest/TabuTestActivity.kt
@@ -24,6 +24,7 @@ class TabuTestActivity : AppCompatActivity() {
         setupRecycler()
         setupQuestions()
         binding.btnSave.setOnClickListener { save() }
+        binding.imageviewBackButton.setOnClickListener { onBackPressed() }
     }
 
     private fun setupRecycler() {
